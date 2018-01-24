a4645ab1af68ae3d22c138241d09d2fa1ecc6ad1
tomas_yang <2310384192@qq.com>
2018-01-24 21:39:10 +0800

    完成逆转字符串

--- 1/1/1/main.cpp
+++ 1/1/1/main.cpp
@@ -1,21 +1,17 @@
 //逆转字符串——输入一个字符串，将其逆转并输出。
 #include<iostream>
 #include<string>
+#include <locale.h>
 using namespace std;
 int main()
 {
-	string str,str1;
-	cin >> str;
-	int str_number = str.size();
-	cout << str_number<<endl;
-	
-	for (int i=0;i<str_number;i++)
-	{
-		cout << i << str[i] <<endl;
-		str1=str1+str[str_number-i-1];
-	}
-	cout << str1<<endl;
+	//wstring 宽字符，每个字符不论中英文都占两个字节，只是对应cin，cout要改成相应命令
+	wcin.imbue(std::locale("chs"));
+	wcout.imbue(std::locale("chs"));
+	wstring str;
+	wcin >> str;
+	wstring str_copy(str.rbegin(), str.rend());
+	wcout << str_copy << endl;
 	return 0;
-//TODO(tomas 18_1_23)：判断是否是中文字符串，以及中文如何逆转输出
 }
 
